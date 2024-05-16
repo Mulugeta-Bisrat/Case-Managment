b2600a4f336d4d2597371949d2b975bf49600610
Mulugeta-Bisrat <34776292+Mulugeta-Bisrat@users.noreply.github.com>
2024-05-15 22:43:03 -0700

    Delete src directory

--- src/main/java/com/acm/casemanagement/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.acm.casemanagement.repository;
-
-import com.acm.casemanagement.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}

02a77627a5046a4b77e37bd6872964b87edb279d
Mulugeta Bisrat <mulugetabisrat@Mulugetas-MBP.lan>
2024-05-18 14:02:00 -0700

    intitial commit

---
+++ src/main/java/com/acm/casemanagement/dto/LoginDto.java
@@ -0,0 +1,11 @@
+package com.acm.casemanagement.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class LoginDto {
+    private String username;
+    private String password;
+}
---
+++ src/test/resources/data.sql
@@ -0,0 +1,10 @@
+CREATE TABLE USERS (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+

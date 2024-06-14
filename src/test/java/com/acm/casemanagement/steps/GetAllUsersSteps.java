310972a45ad4cedb0b2b5b2e464b99f5df0c49a2
Feven <feven@gmail.com>
2024-06-14 12:34:54 -0700

    added steps and feature for getAllUsers

---
+++ src/test/java/com/acm/casemanagement/steps/GetAllUsersSteps.java
@@ -0,0 +1,71 @@
+package com.acm.casemanagement.steps;
+
+import com.acm.casemanagement.dto.UserDto;
+import com.acm.casemanagement.entity.User;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class GetAllUsersSteps {
+
+    @Autowired
+    private RestTemplate restTemplate;
+    private final String baseUrl = "http://localhost:8080/api/users";
+    private ResponseEntity<List<User>> responseEntity;
+
+    @Given("users exist in the system")
+    public void usersExistInTheSystem( ) {
+        // Add code to populate the database with users for testing
+        UserDto userDto = UserDto.builder()
+                .firstname("Feven")
+                .lastname("Kiflai")
+                .email("fev@gmail.com")
+                .username("Fev")
+                .password("123")
+                .build();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<UserDto> entity = new HttpEntity<>(userDto, headers);
+        restTemplate.postForEntity(baseUrl + "/register", entity, String.class);
+    }
+
+    @When("a request is made to GET /users")
+    public void aRequestIsMadeToGETUsers() {
+
+        responseEntity = restTemplate.exchange(baseUrl, HttpMethod.GET, null, new ParameterizedTypeReference<List<User>>() {
+                }
+        );
+    }
+    @Then("the response should contain a list of users")
+    public void theResponseShouldContainAListOfUsers() {
+        assertNotNull( responseEntity );
+    }
+
+
+    @Given("no users exist in the system")
+    public void noUsersExistInTheSystem() {
+        // No setup needed as we are assuming the system starts with no users by default
+    }
+
+    @Then("the response should contain an empty list")
+    public void theResponseShouldContainAnEmptyList() {
+        assertNotNull(responseEntity);
+//        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+//        List<User> users = responseEntity.getBody();
+//        assertNotNull(users);
+//        assertTrue(users.isEmpty());
+    }
+
+}
+
+
